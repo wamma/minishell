2271ba2df57dff8ba00f79cfaacc76cf59f3af69
Hyungjun <pparkheong96@naver.com>
2023-06-19 21:57:37 +0900

    parse_quote.c 구현중

--- srcs/parse_quote.c
+++ srcs/parse_quote.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   parse_quote.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ricecha <ricecha@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hyungjup <hyungjup@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 09:31:17 by seocha            #+#    #+#             */
-/*   Updated: 2023/06/02 22:09:43 by ricecha          ###   ########.fr       */
+/*   Updated: 2023/06/19 21:57:20 by hyungjup         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char	*separate_single(char *original, int i, int j)
+char	**separate_single(char *original, int i, int j)
 {
 	char	**result;
 
@@ -21,18 +21,18 @@ char	*separate_single(char *original, int i, int j)
 		return (NULL);
 	result[0] = ft_substr(original, 0, i);
 	if (result[0] == NULL)
-		return(모르겠음) 
-	result[1] = ft_substr(original, i + 1, j - i - 1)
+		return (NULL);
+	result[1] = ft_substr(original, i + 1, j - i - 1);
 	if (result[1] == NULL)
-		return(모르겠음)
+		return (NULL);
 	result[2] = ft_substr(original, j + 1, ft_strlen(original));
 	if (result[2] == NULL)
-		return(모르겠음)
+		return (NULL);
 	result[3] = NULL;
 	return (result);
 }
 
-char	*separate_double(char *original, int i, int j)
+char	**separate_double(char *original, int i, int j)
 {
 	char	**result;
 
@@ -41,21 +41,21 @@ char	*separate_double(char *original, int i, int j)
 		return (NULL);
 	result[0] = ft_substr(original, 0, i);
 	if (result[0] == NULL)
-		return(모르겠음) 
-	result[1] = ft_substr(original, i + 1, j - i - 1)
+		return (NULL);
+	result[1] = ft_substr(original, i + 1, j - i - 1);
 	if (result[1] == NULL)
-		return(모르겠음)
+		return (NULL);
 	result[2] = ft_substr(original, j + 1, ft_strlen(original));
 	if (result[2] == NULL)
-		return(모르겠음)
+		return (NULL);
 	result[3] = NULL;
-	return (result);	
+	return (result);
 }
 
-char	**separate_quote(char *original) //ex) echo "ls -l" 'dd' -> 'echo ', 'ls -l', 'NULL' , 'NULL' 
+char	**separate_quote(char *original) //ex) echo "ls -l" 'dd' -> 'echo ', 'ls -l', 'NULL' , 'NULL'
 {
 	int	i;
-	int j;
+	int	j;
 
 	i = 0;
 	while (original[i] != '\0')
@@ -67,17 +67,17 @@ char	**separate_quote(char *original) //ex) echo "ls -l" 'dd' -> 'echo ', 'ls -l
 		j = i;
 		if (original[i] == '\'')
 		{
-			while (original[j] != '\0' && orignal[j] != '\'')
+			while (original[j] != '\0' && original[j] != '\'')
 				j++;
 			if (original[j] == '\'')
 				return (separate_single(original, i, j));
 		}
 		if (original[i] == '\"')
 		{
-			while (original[j] != '\0' && orignal[j] != '\"')
+			while (original[j] != '\0' && original[j] != '\"')
 				j++;
 			if (original[j] == '\"')
-				return (seperate_double(originalm i, j));
+				return (seperate_double(original, i, j));
 		}
 	}
 	return (NULL);
@@ -95,6 +95,7 @@ TOKEN_ORIGIN ex) echo "ls -l" 'dd' cs "laaa"
 NULL laaaa NULL
 */
 
+/*원래 코드
 void	parse_quote(t_token *token)
 {
 	char	**sep;
@@ -110,5 +111,50 @@ void	parse_quote(t_token *token)
 				node2 = TOKEN_ARGV;
 			}
 		}
+	}
+}*/
+
+void	parse_quote(t_token *token)
+{
+	char	**sep;
+	t_token	*node1;
+	t_token	*node2;
+	t_token	*new_tokens;
+	t_token	*prev_token;
+
+	new_tokens = NULL;
+	prev_token = NULL;
+	while (token != NULL)
+	{
+		if (token->type == TOKEN_ORIGIN)
+		{
+			sep = separate_quote(token->original);
+			if (sep != NULL)
+			{
+				// 분리된 서브스트링을 처리합니다
+				// ex) echo "ls -l" 'dd' -> 'echo ', 'ls -l', 'NULL', 'NULL'
+
+				// 첫 번째 서브스트링은 원래 토큰의 역할을 유지합니다 (TOKEN_ORIGIN)
+				node1 = create_node(TOKEN_ORIGIN, sep[0], NULL);
+				if (new_tokens == NULL)
+				{
+					new_tokens = node1;
+					prev_token = node1;
+				}
+				else
+				{
+					prev_token->next = node1;
+					prev_token = node1;
+				}
+
+				// 두 번째 서브스트링은 ARGV 역할을 갖습니다(TOKEN_ARGV)
+				node2 = create_node(TOKEN_ARGV, sep[1], NULL);
+				prev_token->next = node2;
+				prev_token = node2;
+
+				// 필요에 따라 추가 서브스트링을 처리합니다
+			}
+		}
+		token = token->next;
 	}
 }
--- srcs/parse_token.c
+++ srcs/parse_token.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   parse_token.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ricecha <ricecha@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hyungjup <hyungjup@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 20:57:46 by ricecha           #+#    #+#             */
-/*   Updated: 2023/06/02 22:14:48 by ricecha          ###   ########.fr       */
+/*   Updated: 2023/06/19 21:22:38 by hyungjup         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 t_token   *parse_main(char *str)
 {
-    # token 순서대로 쪼개기
-    t_token *token;
+	# token 순서대로 쪼개기
+	t_token *token;
 
-    if (token == NULL)
-        return (NULL);
-    token->type = TOKEN_ORIGIN;
-    token->original = str;
-    parse_quote(token);
-    parse_env(token);
-    parse_space(token);
-    parse_pipeline(token);
-    parse_redirection(token);
-    parse_delete_empty_chunk(token);
-    parse_chunk_to_argv(token);
-    parse_merge_argv(token);
-    parse_delete_space(token);
+	if (token == NULL)
+		return (NULL);
+	token->type = TOKEN_ORIGIN;
+	token->original = str;
+	parse_quote(token);
+	parse_env(token);
+	parse_space(token);
+	parse_pipeline(token);
+	parse_redirection(token);
+	parse_delete_empty_chunk(token);
+	parse_chunk_to_argv(token);
+	parse_merge_argv(token);
+	parse_delete_space(token);
 }
